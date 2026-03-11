c05b1f8e7fba7098514ebc51ae52eee8de7310bc
Danihabtu <dhabtu95@gmail.com>
2026-03-11 13:58:26 +0300

    Initial commit

---
+++ backend/routes/workouts.js
@@ -0,0 +1,17 @@
+const express=require("express")
+const Workout=require('../models/workoutModel')
+const router=express.Router()
+const {createWorkout,
+    getWorkouts,
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
+}=require("../controllers/workoutController")
+router.get("/",getWorkouts)
+router.get('/:id',getWorkout)
+router.post('/',createWorkout)
+router.delete('/:id',deleteWorkout)
+  
+router.patch('/:id',updateWorkout)
+
+module.exports=router
